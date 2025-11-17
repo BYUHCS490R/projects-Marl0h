54a59e98d514a98cf238bec8175e52dc5ff1dd07
Marloh <rylanmarler97@gmail.com>
2025-11-17 11:28:36 -1000

    bug fixes

--- activities/form.js
+++ activities/form.js
@@ -17,7 +17,6 @@ document.getElementById('myForm').addEventListener('submit',function(event) {
         email: email,
         password: password,
     }
-    alert("Form Submitted");
     console.log(formData);
 
     const xhr = new XMLHttpRequest();
